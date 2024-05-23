58fb099e9caed066ba37bad56a5f84a13a52be3e
Bennett Fife <bennett.fife@gmail.com>
2024-05-23 03:58:37 -0700

    image picker implementation

--- app/index.js
+++ app/index.js
@@ -1,14 +1,43 @@
-import { View, Text, Button, StyleSheet, Image } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, Image, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import { Link } from "expo-router";
 
 const HomePage = () => {
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === "granted");
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  if (hasGalleryPermission === false) {
+    return <Text>No access to Internal Storage</Text>;
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Image
-        style={{ margin: 25, width: 100, height: 100, borderRadius: 50 }}
-        source={require("../assets/bennett.png")}
-      />
+      <View style={{ alignItems: "center" }}>
+        {image && <Image style={styles.image} source={{ uri: image }} />}
+        <Button title="Change Profile Pic" onPress={() => pickImage()} style={styles.button} />
+      </View>
       <Text style={{ fontSize: 30, marginBottom: 25 }}>Bennett's Portfolio</Text>
       <Link style={styles.linkButton} href={"/projects"}>
         Projects Page
@@ -24,11 +53,33 @@ const HomePage = () => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
   linkButton: {
-    padding: 10,
-    margin: 10,
+    fontSize: 20,
+    margin: 5,
     borderColor: "black",
     borderWidth: 1,
+    padding: 5,
+  },
+  image: {
+    marginTop: 25,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
   },
 });
 
